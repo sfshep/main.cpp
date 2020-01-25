7c03e50c290b1dc34dc50b91ad7ea6bf0fa75f73
sfshep <stewart.sheppard@my.tccd.edu>
2020-01-25 05:16:59 +0000

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-/* 
+/*  Stewart Sheppard, COSC 1436-57002
+    Text Editor: Repl / GitHub  
+    This program allows my friend Joe to enter in the number of pennies before cashing them in at the bank.  By doing so, he can confirm that he receives the right number of bills and coins in return.
 
 */
 
