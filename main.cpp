c9a0ff96d7de107ff15a1219967f3168b79481fc
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-01-25 18:26:07 +0000

    Added //Input comment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*  Stewart Sheppard, COSC 1436-57002
-    Text Editor: Repl / GitHub  
+    Text Editor: Repl.it / GitHub  
     This program allows my friend Joe to enter in the number of pennies before cashing them in at the bank.  By doing so, he can confirm that he receives the right number of bills and coins in return.
 */
 #include <iostream>
@@ -13,8 +13,10 @@ int numDollars = 0;
 int numDollarRemainder = 0;
 int numQuarters = 0; 
 int numQuarterRemainder = 0;
+// trying a combined format of declaring and initializing in one line
 int numDimes = 0, numDimesRemainder = 0, numNickels = 0, numNickelsRemainder  = 0;
 
+//Input
 //Ask Joe to enter in the number of pennies
 cout << "Joe, please enter in the number of pennies:  ";
 cin >> numPennies;
@@ -24,11 +26,20 @@ numDollars = numPennies / 100;
 numDollarRemainder = numPennies % 100;
 numQuarters = numDollarRemainder / 25;
 numQuarterRemainder = numDollarRemainder % 25;
-
+numDimes = numQuarterRemainder / 10;
+numDimesRemainder = numQuarterRemainder % 10;
+numNickels = numDimesRemainder / 5;
+numNickelsRemainder= numDimesRemainder % 5;
 
 
 //Output
 cout << "\n Dollars = " << numDollars << endl;
 cout << "\n Quarters = " << numQuarters << endl;
+cout << "\n Dimes = " << numDimes << endl;
+cout << "\n Nickels = " << numNickels << endl;
+cout << "\n Pennies = " << numNickelsRemainder << endl;
+
+ 
+ 
   return 0;
 }
